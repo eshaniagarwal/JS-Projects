a7bfd7821bc72becd06473a9563f1406526220c1
eshaniagarwal <49741412+eshaniagarwal@users.noreply.github.com>
2024-05-27 10:27:25 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+//alert('hello') //we are using nodejs not browser
+
+
+//console.log(3+3) console.log('eshani') //gives error
+console.log(3+3); console.log('eshani')
+
+let name ='eshani'
+let age =25
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean =? true/false
+// null => standalone value i.e represenation of empty value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof "")//string
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
---
+++ basics/variable.js
@@ -0,0 +1,17 @@
+const accountId = 3223123
+//accountId = 5 //Not allowed
+//console.log(accountId)
+
+let accountEmail = 'xyz@gmail.com'
+var accountPassword = 'sfssds'
+accountCity = 'Meerut'
+let accountState;
+
+accountEmail = 'abc@gmail.com'
+accountPassword = 'test'
+accountCity = 'Delhi'
+console.table([accountEmail,accountPassword,accountId,accountCity,accountState])
+
+/*
+Prefer not to use var because of issue in block and functional scope
+*/
